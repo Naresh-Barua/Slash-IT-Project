4ab2587d2a5b2122cf8ba6d526ae783f2d7ae84f
Naresh Barua <69834028+Naresh-Barua@users.noreply.github.com>
2023-09-28 03:52:37 +0600

    Done

---
+++ src/components/Navbar/Mylinks.jsx
@@ -0,0 +1,85 @@
+export const links = [
+    {
+      name: "Settings",
+      submenu: true,
+      sublinks: [
+        {
+          Head: "Profile",
+          sublink: [
+            { name: "Change Username", link: "/" },
+            { name: "Change Password", link: "/" },
+            { name: "Change Profile Picture", link: "/" },
+          ],
+        },
+        {
+          Head: "Pages",
+          sublink: [
+            { name: "My Page", link: "/" },
+            { name: "Manage Page", link: "/" },
+            { name: "Manage Post", link: "/" },
+          ],
+        },
+       
+      ],
+    },
+    {
+      name: "Services",
+      submenu: true,
+      sublinks: [
+        {
+          Head: "Email Marketing",
+          sublink: [
+            { name: "Newsletters", link: "/" },
+            { name: "Campaign Management", link: "/" },
+            { name: "Templates", link: "/" },
+          ],
+        },
+        {
+          Head: " SEO (Search Engine Optimization)",
+          sublink: [
+            { name: "Site Audit", link: "/" },
+            { name: "Keyword Research", link: "/" },
+            { name: "On-page Optimization", link: "/" },,
+          ],
+        },
+        {
+          Head: "Google Ad Sense",
+          sublink: [
+            { name: "Setup & Integration", link: "/" },
+            { name: "Optimization", link: "/" },
+            { name: "Ad Layouts", link: "/" },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Contact Us",
+      submenu: true,
+      sublinks: [
+        {
+          Head: "Email",
+          sublink: [
+            { name: "example@example.com", link: "/" },
+            { name: "support@example.com", link: "/" },
+            { name: "sales@example.com", link: "/" },
+          ],
+        },
+        {
+          Head: "Phone",
+          sublink: [
+            { name: "Main Line: +1-123-456-7890", link: "/" },
+            { name: "Support: +1-123-456-7891", link: "/" },
+            { name: "Sales: +1-123-456-7892", link: "/" },
+          ],
+        },
+        {
+          Head: "Physical Address",
+          sublink: [
+            { name: "Main Office: 123 Main St, City, Country", link: "/" },
+            { name: "Regional Office: 456 Regional Rd, City, Country", link: "/" },
+          ],
+        },
+  
+      ],
+    },
+  ];
---
+++ src/components/Slider/sliderImage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+import First from '../../assests/First.jpg';
+import Second from '../../assests/Second.jpg';
+import Third from '../../assests/Third.jpg';
+export default [
+  {
+    
+    urls: First,
+  },
+  {
+    
+    urls: Second,
+  },
+  {
+   
+    urls: Third,
+  },
+  
+];
+
